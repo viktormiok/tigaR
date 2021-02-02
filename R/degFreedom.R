cbd4f75878130f74fc50acf454410cc0fdcec8de
viktormiok <viktormiok@gmail.com>
2021-02-02 12:15:59 +0100

    initial commit

---
+++ R/degFreedom.R
@@ -0,0 +1,42 @@
+###############################################################################
+#
+#         calculate degrees of freedom used in the testing
+#
+###############################################################################
+
+degFreedom <- function(fit, 
+                       design,
+                       ZSpline){
+  # fit: Fitted object of the full model
+  # design: Design matrix for replicates
+  # ZSpline:  Design matrix for splines
+  if (!is(fit, "list")) {
+    stop("Input (fit) is of wrong class.")
+  }
+  if (!is(design, "matrix")) {
+    stop("Input (design) is of wrong class.")
+  }
+  if (!is(ZSpline, "matrix")) {
+    stop("Input (ZSpline) is of wrong class.")
+  }
+  if (nrow(ZSpline) != nrow(design)) {
+    stop("Dimension of the design matrices do not correspond")
+  }
+  dMat <- cbind(design, ZSpline)
+  I <- diag(ncol(dMat))
+  m_e <- inla.emarginal(function(x) 1/sqrt(x),
+                        fit$marginals.hyper[[2]]
+  )
+  m_b <- inla.emarginal(function(x) sqrt(x),
+                        fit$marginals.hyper[[1]]
+  )
+  lambda <- m_e*m_b
+  if(is.null(lambda)){
+    print("Problem problem with estimation")}
+  else{        
+    df <- sum(diag(dMat %*% solve(t(dMat) %*% dMat + 
+                                    lambda*I) %*% t(dMat))) -
+      (ncol(dMat) - ncol(ZSpline))
+    return(df)
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# tigaR
-Temporal Integrative (Gen)omics Analysis in R 
+# Sope
+
+The R-package tigaR performs temporal integrative differential expression analysis of omics data, both sequencing counts RNAseq and continuous microarray data.
+
+# Insatllation
+
+The package tigaR depends on ShrinkBayes and on R >= 3.0.0 and is available from GitHub. This requires the package devtools:
+
+# Reference
+
+Miok, V., Wilting, S.M., Van de Wiel, M.A., Jaspers, A., van Noort, P.I., Brakenhoff, R.H., Snijders, P.J.F., Steenbergen, R.D.M., Van Wieringen, W.N. (2014), "tigaR: integrative significance analysis of temporal differential gene expression induced by genomic abnormalities", BMC Bioinformatics, 15, 327.
